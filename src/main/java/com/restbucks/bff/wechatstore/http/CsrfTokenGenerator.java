1589519bae02bd519be6c25007bfdc6c77876b77
hippoom <hippoom@gmail.com>
2016-11-06 22:02:25 +0800

    implements CsrfTokenGenerator #1

--- src/main/java/com/restbucks/bff/wechatstore/http/CsrfTokenGenerator.java
+++ src/main/java/com/restbucks/bff/wechatstore/http/CsrfTokenGenerator.java
@@ -1,7 +1,15 @@
 package com.restbucks.bff.wechatstore.http;
 
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.stereotype.Component;
+
+import java.security.SecureRandom;
+
+@Component
 public class CsrfTokenGenerator {
     public String generate() {
-        return null;
+        byte[] r = new byte[256]; //Means 2048 bit
+        new SecureRandom().nextBytes(r);
+        return Base64.encodeBase64String(r);
     }
 }
