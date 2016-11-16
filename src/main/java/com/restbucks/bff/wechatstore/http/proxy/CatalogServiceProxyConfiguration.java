5ec7c1e5a4384088b31411febeb74c264bca1775
hippoom <hippoom@gmail.com>
2016-11-16 21:17:14 +0800

    proxy catalogs to catalogs service #2

---
+++ src/main/java/com/restbucks/bff/wechatstore/http/proxy/CatalogServiceProxyConfiguration.java
@@ -0,0 +1,31 @@
+package com.restbucks.bff.wechatstore.http.proxy;
+
+import org.mitre.dsmiley.httpproxy.ProxyServlet;
+import org.springframework.boot.bind.RelaxedPropertyResolver;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class CatalogServiceProxyConfiguration implements EnvironmentAware {
+
+    private RelaxedPropertyResolver propertyResolver;
+
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new ProxyServlet(),
+                propertyResolver.getProperty("mapping"));
+        servletRegistrationBean.addInitParameter("targetUri",
+                propertyResolver.getProperty("target"));
+        servletRegistrationBean.addInitParameter(ProxyServlet.P_LOG,
+                propertyResolver.getProperty("logging", "false"));
+        return servletRegistrationBean;
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.propertyResolver = new RelaxedPropertyResolver(environment, "proxy.catalogService.");
+    }
+}
